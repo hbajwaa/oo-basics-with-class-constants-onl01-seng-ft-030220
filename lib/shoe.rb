72d60b2bcde38002287031c9671e2f05bfc82375
Hassan Bajwa <bajwahassan3@outlook.com>
2020-03-12 09:24:22 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,12 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS
+  
   def initialize(brand)
     @brand = brand
+    
+    BRANDS << brand if BRANDS.!include(brand)
   end
 
   def cobble
@@ -11,13 +15,6 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
-  BRANDS = []
-  def brand=(brand)
-    @brand = brand
-    if BRANDS.include?(brand)
-    else
-      BRANDS << brand
-    end
-  end
+  
   
 end
